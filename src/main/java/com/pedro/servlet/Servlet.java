459b27231341b5aceb5d8da2290e0be4f83131ef
pedrofabian455 <pedrofabianzarate@gmail.com>
2023-04-12 20:55:21 -0400

    Servlet doGet

---
+++ src/main/java/com/pedro/servlet/Servlet.java
@@ -0,0 +1,29 @@
+package com.pedro.servlet;
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Fabian
+ */ 
+
+@WebServlet(urlPatterns="/inicio")
+public class Servlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        resp.getWriter().append("Hola Mundo!") ;
+    }
+    
+}
